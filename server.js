752d12db13f02f5d60e23fda6f0a15310fae868c
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-08-13 15:09:44 -0400

    configure basic server with test route

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || '8080');
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get('/api/test', (req, res) => {
+  res.json({
+    user: 'Ryo',
+    message: 'Well, this seemed to have worked.',
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port ${app.get('port')}`);
+});
