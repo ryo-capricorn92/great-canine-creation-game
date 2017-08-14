41ef42a892785c3a285ba8fd36bee43f2457ef3d
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-08-14 16:30:56 -0400

    setup basic passport configuration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const session = require('express-session');
 const db = require('./server/db');
 const routes = require('./server/routes');
+const passport = require('passport');
 
 /* create express server */
 const app = express();
@@ -12,6 +14,18 @@ app.set('port', process.env.PORT || '8080');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+/* configure middleware and express configs */
+app.use(session({
+  secret: 'piano possum',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 604800000,
+  },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* define all the routes */
 routes(app, express);
