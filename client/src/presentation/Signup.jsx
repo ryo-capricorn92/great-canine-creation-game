5c6d57024452da37b6bb6dc5eb4839b961b3f6e0
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-08-24 15:19:13 -0400

    don't signup if any field is wrong and show an error

--- client/src/presentation/Signup.jsx
+++ client/src/presentation/Signup.jsx
@@ -6,6 +6,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Label = styled.label`
@@ -43,11 +44,20 @@ const Icon = styled.i`
   }
 `;
 
+const Error = styled.div`
+  color: red;
+  background-color: #e5cece;
+  border-radius: 3px;
+  padding: 5px;
+  margin-top: 10px;
+`;
+
 class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
+      message: '',
       status: {
         username: '',
         email: '',
@@ -57,20 +67,49 @@ class Signup extends React.Component {
       },
     };
 
+    this.onSubmit = this.onSubmit.bind(this);
     this.checkUsername = this.checkUsername.bind(this);
     this.checkEmail = this.checkEmail.bind(this);
     this.checkConfirmEmail = this.checkConfirmEmail.bind(this);
     this.checkPassword = this.checkPassword.bind(this);
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    const isInvalid = (bool, key) => bool || this.state.status[key] === 'close';
+    if (Object.keys(this.state.status).reduce(isInvalid, false)) {
+      this.setState({ message: 'Please address the issues before submitting the form' });
+      return false;
+    }
+
+    const user = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+      email: event.target.email.value,
+      birthday: new Date(event.target.birthday.value),
+    };
+
+    fetch('/api/signup', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+    }).then((res) => {
+      console.log(res.json());
+    });
+  }
+
   checkUsername(event) {
+    const message = '';
     let status = Object.assign({}, this.state.status);
     if (!event.target.value.trim()) {
       status.username = '';
-      this.setState({ status });
+      this.setState({ message, status });
     } else {
       status.username = 'circle-o-notch fa-spin';
-      this.setState({ status });
+      this.setState({ message, status });
 
       fetch(`/check/username/${event.target.value}`)
         .then(res => res.json())
@@ -82,20 +121,21 @@ class Signup extends React.Component {
           } else {
             status.username = 'check';
           }
-          this.setState({ status });
+          this.setState({ message, status });
         });
     }
   }
 
   checkEmail(event) {
     const email = event.target.value;
+    const message = '';
     let status = Object.assign({}, this.state.status);
     if (!event.target.value.trim()) {
       status.email = '';
-      this.setState({ email, status });
+      this.setState({ email, message, status });
     } else {
       status.email = 'circle-o-notch fa-spin';
-      this.setState({ email, status });
+      this.setState({ email, message, status });
 
       fetch(`/check/email/${email}`)
         .then(res => res.json())
@@ -107,12 +147,13 @@ class Signup extends React.Component {
           } else {
             status.email = 'check';
           }
-          this.setState({ email, status });
+          this.setState({ email, message, status });
         });
     }
   }
 
   checkConfirmEmail(event) {
+    const message = '';
     const status = Object.assign({}, this.state.status);
     if (this.state.status.email === 'close' || !event.target.value.trim()) {
       status.emailConfirm = '';
@@ -121,10 +162,11 @@ class Signup extends React.Component {
     } else {
       status.emailConfirm = 'close';
     }
-    this.setState({ status });
+    this.setState({ message, status });
   }
 
   checkPassword(event) {
+    const message = '';
     const status = Object.assign({}, this.state.status);
     if (!event.target.value.trim()) {
       status.password = '';
@@ -133,40 +175,13 @@ class Signup extends React.Component {
     } else {
       status.password = 'close';
     }
-    this.setState({ status });
+    this.setState({ message, status });
   }
 
   render() {
-    const onSubmit = (event) => {
-      event.preventDefault();
-
-      if (event.target.email.value !== event.target.email_confirm.value) {
-        console.log('Emails don\'t match');
-        return;
-      }
-
-      const user = {
-        username: event.target.username.value,
-        password: event.target.password.value,
-        email: event.target.email.value,
-        birthday: new Date(event.target.birthday.value),
-      };
-
-      fetch('/api/signup', {
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: 'same-origin',
-      }).then((res) => {
-        console.log(res.json());
-      });
-    };
-
     return (
       <Container>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={this.onSubmit}>
           <table>
             <tbody>
               <tr>
@@ -221,6 +236,7 @@ class Signup extends React.Component {
             </tbody>
           </table>
         </form>
+        {this.state.message ? (<Error>{this.state.message}</Error>) : undefined}
       </Container>
     );
   }
