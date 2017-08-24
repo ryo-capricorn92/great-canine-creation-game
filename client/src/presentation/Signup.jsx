93d1961b3c7018ebfb22b97ec0fa79bfe0487ae3
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-08-24 11:39:43 -0400

    add basic signup functionality without validation

--- client/src/presentation/Signup.jsx
+++ client/src/presentation/Signup.jsx
@@ -33,14 +33,46 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      usernameConfirm: false,
+      username: '',
+      usernameUsed: false,
+      email: '',
+      confirmEmail: '',
+      emailUsed: false,
+      birthday: '',
     };
   }
 
   render() {
+    const onSubmit = (event) => {
+      event.preventDefault();
+
+      if (event.target.email.value !== event.target.email_confirm.value) {
+        console.log('Emails don\'t match');
+        return;
+      }
+
+      const user = {
+        username: event.target.username.value,
+        password: event.target.password.value,
+        email: event.target.email.value,
+        birthday: new Date(event.target.birthday.value),
+      };
+
+      fetch('/api/signup', {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+      }).then((res) => {
+        console.log(res.json());
+      });
+    };
+
     return (
       <Container>
-        <form>
+        <form onSubmit={onSubmit}>
           <table>
             <tbody>
               <tr>
@@ -48,7 +80,7 @@ class Signup extends React.Component {
                   <Label htmlFor="username">Username:</Label>
                 </LabelCell>
                 <InputCell>
-                  <input name="username" />
+                  <input name="username" required />
                 </InputCell>
               </tr>
               <tr>
@@ -56,7 +88,7 @@ class Signup extends React.Component {
                   <Label htmlFor="password">Password:</Label>
                 </LabelCell>
                 <InputCell>
-                  <input name="password" type="password" />
+                  <input name="password" type="password" required />
                 </InputCell>
               </tr>
               <tr>
@@ -64,7 +96,7 @@ class Signup extends React.Component {
                   <Label htmlFor="email">Email:</Label>
                 </LabelCell>
                 <InputCell>
-                  <input name="email" type="email" />
+                  <input name="email" type="email" required />
                 </InputCell>
               </tr>
               <tr>
@@ -72,7 +104,7 @@ class Signup extends React.Component {
                   <Label htmlFor="email_confirm">Confirm Email:</Label>
                 </LabelCell>
                 <InputCell>
-                  <input name="email_confirm" type="email" />
+                  <input name="email_confirm" required />
                 </InputCell>
               </tr>
               <tr>
@@ -80,7 +112,7 @@ class Signup extends React.Component {
                   <Label htmlFor="birthday">Birthday:</Label>
                 </LabelCell>
                 <InputCell>
-                  <input name="birthday" type="date" />
+                  <input name="birthday" type="date" required />
                 </InputCell>
               </tr>
               <tr>
