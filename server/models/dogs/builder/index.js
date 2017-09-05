a9035e3e335314cfea86fdaebe27757dd98a422d
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-09-05 12:58:34 -0400

    add random physique generation

--- server/models/dogs/builder/index.js
+++ server/models/dogs/builder/index.js
@@ -1,5 +1,6 @@
 const locus = require('./locus');
 const rcm = require('./mutant-rcm');
+const physique = require('./physique');
 
 function Builder(dam, sire) {
   this.dam = dam;
@@ -9,10 +10,12 @@ function Builder(dam, sire) {
 
 Object.assign(Builder.prototype, locus);
 Object.assign(Builder.prototype, rcm);
+Object.assign(Builder.prototype, physique);
 
 Builder.prototype.buildRandomDog = function buildRandomDog() {
   this._generateRandomLocus();
   this._generateRandomRCM();
+  this._generateRandomPhysique();
 };
 
 module.exports = Builder;
--- server/models/dogs/builder/physique.js
+++ server/models/dogs/builder/physique.js
@@ -167,8 +167,82 @@ const PHYSIQUE = {
   },
 };
 
+function randomInt(min, max) {
+  return Math.floor((Math.random() * ((max - min) + 1)) + min);
+}
+
+function sortByDominance(locus) {
+  return (a, b) => {
+    if (locus[a].dominantTo.includes(b)) {
+      return -1;
+    }
+    if (locus[b].dominantTo.includes(a)) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    return 1;
+  };
+}
+
+function genLocus(locus) {
+  const alleles = Object.keys(locus);
+  const alleleA = alleles[Math.floor(Math.random() * alleles.length)];
+  const alleleB = alleles[Math.floor(Math.random() * alleles.length)];
+  const pair = `${alleleA}${alleleB}`;
+  return pair
+    .split('')
+    .sort(sortByDominance(locus))
+    .join('');
+}
+
 module.exports = {
   _generateRandomPhysique() {
+    const { sizing, typing } = PHYSIQUE;
     this.physique = {};
+
+    /* BODY */
+    this.physique.body = {};
+    this.physique.body.size = randomInt(sizing.body.size[0], sizing.body.size[1]);
+    this.physique.body.gene = genLocus(typing.body);
+
+    /* COAT */
+    this.physique.coat = {};
+    this.physique.coat.curl = genLocus(typing.coat.curl);
+    this.physique.coat.furnishings = genLocus(typing.coat.furnishings);
+    this.physique.coat.long = genLocus(typing.coat.long);
+    this.physique.coat.wire = genLocus(typing.coat.wire);
+    this.physique.coat.hairless = genLocus(typing.coat.hairless);
+
+    /* DEWCLAWS */
+    this.physique.dewclaws = {};
+    this.physique.dewclaws.gene = genLocus(typing.dewclaws);
+
+    /* EARS */
+    this.physique.ears = {};
+    this.physique.ears.size = randomInt(sizing.ears.size[0], sizing.ears.size[1]);
+    this.physique.ears.gene = genLocus(typing.ears);
+
+    /* EYES */
+    this.physique.eyes = {};
+    this.physique.eyes.gene = genLocus(typing.eyes);
+
+    /* HEAD */
+    this.physique.head = {};
+    this.physique.head.size = randomInt(sizing.head.size[0], sizing.head.size[1]);
+
+    /* LEGS */
+    this.physique.legs = {};
+    this.physique.legs.size = randomInt(sizing.legs.size[0], sizing.legs.size[1]);
+
+    /* MUZZLE */
+    this.physique.muzzle = {};
+    this.physique.muzzle.size = randomInt(sizing.muzzle.size[0], sizing.muzzle.size[1]);
+
+    /* TAIL */
+    this.physique.tail = {};
+    this.physique.tail.size = randomInt(sizing.tail.size[0], sizing.tail.size[1]);
+    this.physique.tail.gene = genLocus(typing.tail);
   },
 };
